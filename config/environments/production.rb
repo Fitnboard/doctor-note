d726569a1ddae35276ded1bcffabe0dda008c77c
Fit <fitnboard@mail.com>
2017-02-22 00:18:05 +0100

    database devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'doctor-note.herokuapp.comyoursite.herokuapp.com' }
 end
